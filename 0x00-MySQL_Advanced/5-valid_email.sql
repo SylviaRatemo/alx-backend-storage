143a24c706b3f3fdad3586ebd88526233f8fc83a
Sylvia Ratemo <ratemosylvia@gmail.com>
2023-10-12 14:53:38 +0300

    email validation

---
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -0,0 +1,17 @@
+-- Trigger
+-- email validation sent
+DROP TRIGGER IF EXISTS valid;
+DELIMITER //
+
+CREATE TRIGGER valid
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF OLD.email != NEW.email THEN
+        SET NEW.valid_email = 0;
+    ELSE
+        SET NEW.valid_email = NEW.valid_email;
+    END IF;
+END //
+
+DELIMITER ;
