45b76cc78de57b2f3f2b774d02a6c2056ca3a958
Sylvia Ratemo <ratemosylvia@gmail.com>
2023-10-12 16:44:36 +0300

    Index

---
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -0,0 +1,7 @@
+-- Stored Procedure
+-- email validation sent
+ALTER TABLE names
+ADD COLUMN first_letter CHAR(1) GENERATED ALWAYS
+    AS (LEFT(name, 1)) STORED;
+
+CREATE INDEX idx_name_first ON names (first_letter);
