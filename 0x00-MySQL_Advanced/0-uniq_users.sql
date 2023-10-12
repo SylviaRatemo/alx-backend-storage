339fec50f7c70b96d300edb775492024a4cac97a
Sylvia Ratemo <ratemosylvia@gmail.com>
2023-10-12 12:03:41 +0300

    uniq users

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- create table user
+-- id, email, name
+CREATE TABLE IF NOT EXISTS users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    name VARCHAR(255)
+    );
