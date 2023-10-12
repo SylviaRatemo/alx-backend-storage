d158e630069a7053ca3e541156633b392cae4bcb
Sylvia Ratemo <ratemosylvia@gmail.com>
2023-10-12 15:28:42 +0300

    more stored procedure

---
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -0,0 +1,25 @@
+-- Stored Procedure
+-- email validation sent
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+DELIMITER //
+
+CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
+BEGIN
+    DECLARE total_score INT DEFAULT 0;
+    DECLARE projects_count INT DEFAULT O;
+ 
+    SELECT SUM(score)
+        INTO total_score
+        FROM corrections
+        WHERE corrections.user_id = user_id;
+    SELECT COUNT(*)
+        INTO projects_count
+        FROM corrections
+        WHERE corrections.user_id = user_id;
+
+    UPDATE users
+        SET users.average_score = IF(projects_count = 0, 0, total_score / projects_count)
+        WHERE users.id = user_id;
+END //
+
+DELIMITER ;
